2002f4007515cdd857a1d8a600919d0e7a72890c
Corentin <corentin@kyxar.fr>
2021-02-04 15:14:58 +0100

    Nouvelles méthodes

--- main.c
+++ main.c
@@ -9,11 +9,13 @@ int main(void) {
     printf("5-4=%d\n", sub(5, 4));
     printf("1x4=%d\n", mul(1, 4));
     printf("4/2=%d\n", div(4, 2));
+    printf("4^2=%d\n", square(4));
     
     printf("1+4=%f\n", fadd(1.0, 4.0));
     printf("5-4=%f\n", fsub(5.0, 4.0));
     printf("1x4=%f\n", fmul(1.0, 4.0));
     printf("2/4=%f\n", fdiv(2.0, 4.0));
+    printf("4.1^2=%f\n", fsquare(4.1));
     
     return 0;
 }
